85d0cf452a79a8fb66258d1940df7df852909a5c
softish <lazar.h.klincov@live.se>
2019-08-24 23:37:19 +0200

    Add test case for updating date modified property of file

--- src/test/java/DateModifiedUpdaterTest.java
+++ src/test/java/DateModifiedUpdaterTest.java
@@ -1,10 +1,12 @@
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class DateModifiedUpdaterTest {
 
@@ -45,5 +47,18 @@ public class DateModifiedUpdaterTest {
         Date expectedDate = new Date();
         // when
         DateModifiedUpdater.createDate(dateFromFileName);
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    public void testUpdateDateModifiedOfFile() {
+        // given
+        File fileMock = Mockito.mock(File.class);
+        Mockito.when(fileMock.getName()).thenReturn("reliccoh-20091012-1648557.jpg");
+        GregorianCalendar expectedTimeStamp = new GregorianCalendar(2009, Calendar.OCTOBER, 12, 16, 48, 55);
+        // when
+        DateModifiedUpdater.updateDateModifiedOfFile(fileMock);
+        // then
+        Mockito.verify(fileMock, Mockito.times(1)).setLastModified(expectedTimeStamp.getTimeInMillis());
     }
 }
